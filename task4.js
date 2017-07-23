36a8ed9590701029e74d6f47604f10d686fa91f5
Idan Mosseri <mosseridan@gmail.com>
2017-07-23 10:53:05 +0000

    task6: UPDATE

--- task4.js
+++ task4.js
@@ -12,18 +12,3 @@ mongo.connect(url, (err, db) => {
            db.close();
         });
 });
-
-
-// var mongo = require('mongodb').MongoClient;
-// var age = Number(process.argv[2]);
-// var url = 'mongodb://localhost:27017/learnyoumongo';
-
-// mongo.connect(url, (err, db) => {
-//     if(err) throw(err);
-//     db.collection('parrots').find({age: {$gt: age}}).toArray((err, parrots) => {
-//         if(err) throw(err);
-//         console.log(parrots);
-//         db.close();
-//     });
-// });
-    
