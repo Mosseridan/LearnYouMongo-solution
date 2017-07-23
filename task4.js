60df1e11043028df2bf57ed6bd708a535f9a2d7c
Idan Mosseri <mosseridan@gmail.com>
2017-07-23 09:27:27 +0000

    task4: FIND PORECT

---
+++ task4.js
@@ -0,0 +1,29 @@
+// FIND PROJECT
+var mongo = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/learnyoumongo';
+var age = Number(process.argv[2]);
+
+mongo.connect(url, (err, db) => {
+    if(err) throw(err);
+    db.collection('parrots').find({age: {$gt: age}}, { _id: 0, name: 1, age: 1})
+        .toArray((err,parrots) => {
+           if(err) throw(err);
+           console.log(parrots);
+           db.close();
+        });
+});
+
+
+// var mongo = require('mongodb').MongoClient;
+// var age = Number(process.argv[2]);
+// var url = 'mongodb://localhost:27017/learnyoumongo';
+
+// mongo.connect(url, (err, db) => {
+//     if(err) throw(err);
+//     db.collection('parrots').find({age: {$gt: age}}).toArray((err, parrots) => {
+//         if(err) throw(err);
+//         console.log(parrots);
+//         db.close();
+//     });
+// });
+    
